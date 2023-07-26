ec466dbb5b88a1a5c65e35f4ec076215cabca27c
arun babu nt <ntarunbabu@gmail.com>
2023-07-26 22:04:26 +0300

    completed the project

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,28 @@
 import random
 
 computer_number = random.randint(1, 10)
-print(computer_number)
+#  print(computer_number)
 guesses = 0
 
 print('You can try five times')
 
-
+while True:
+    try:
+        user_input = int(input('Enter a number through 1 -10'))
+    except ValueError:
+        print('Please enter a number')
+    else:
+        guesses += 1
+        if guesses == 5:
+            print('You reached maximum chance')
+            break
+        elif user_input > computer_number:
+            print(f'Number you entered {user_input} is greater than the number')
+            print(f'you have {5 - guesses} chance left')
+        elif user_input < computer_number:
+            print(f'Number you entered {user_input} is smaller than the number')
+            print(f'you have {5 - guesses} chance left')
+        else:
+            print('You entered the correct number')
+            print(f'you took {guesses} chances'.upper())
+            break
