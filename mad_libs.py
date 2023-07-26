9b1894b4bbb2de23c353fd713fcdefe34c267d92
arun babu nt <ntarunbabu@gmail.com>
2023-07-26 21:14:00 +0300

    completed mad libs program

---
+++ mad_libs.py
@@ -0,0 +1,28 @@
+adjective = input('Enter the adjective!')
+noun = input('Enter the noun!')
+street_name = input('Enter the street name')
+plural_verb = input('Enter the plural verb')
+plural_noun = input('Enter the plural noun')
+name = input('Enter the name')
+verb = input("Enter the verb")
+exclamation = input('Enter the exclamation')
+emotion = input('Enter the emotion')
+
+mad_libs_text = f'''
+Once, in a {adjective} town, there was an abandoned {noun} at the end of {street_name}. 
+Legend had it that the old {noun} was haunted by a {adjective} {noun}. 
+Brave souls who dared to enter reported hearing {noun} {plural_verb} and seeing {adjective} {plural_noun}.
+One {adjective} night, {name} and their friends decided to explore the haunted house.
+Armed with a {noun}, they cautiously entered the creaky {noun}. 
+The floorboards {verb} beneath their feet as they ventured deeper into the {adjective} darkness.
+Suddenly, a {adjective} {noun} appeared out of nowhere, making everyone {verb}. 
+{name} bravely stepped forward, holding the {noun} high, and shouted, "{exclamation}!" 
+Surprisingly, the {noun} retreated, allowing them to continue.
+As they reached the heart of the house, they discovered a hidden {noun}. Inside was a {adjective} {noun},
+which seemed to hold a mysterious power. {name} knew they had to {verb} it.
+With the {noun} in hand, they rushed out of the haunted house, feeling a mix of fear and {emotion}. 
+The town's people were amazed by their bravery and hailed them as heroes.
+
+'''
+
+print('Mad libs:', mad_libs_text)
